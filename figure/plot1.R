e876ad51ef62a8cef9df5cc5a7bbb49192e5ddf7
Piano2468 <emily.l.henry@gmail.com>
2014-11-04 11:16:24 -0500

    Update plot1.R

--- figure/plot1.R
+++ figure/plot1.R
@@ -1,4 +1,3 @@
-setwd("~/All ELB Work Documents/R Working Directory")
 HHPower <- read.table("household_power_consumption.txt", sep=";", header = TRUE)
 HHPower2 <- subset(HHPower, HHPower$Date == "1/2/2007" | HHPower$Date == "2/2/2007")
 HHPower2$Global_active_power<-as.numeric(as.character(HHPower2$Global_active_power))
